84daa150ee42f2adf927fcf3482c5f4f1ef65555
devYuMinKim <gimyumin40@gmail.com>
2023-06-28 16:02:38 +0900

    ✨ feat : Implement restaurant search based on user's location

--- app/Services/RecruitApiService.php
+++ app/Services/RecruitApiService.php
@@ -15,10 +15,13 @@ class RecruitApiService
     ]);
   }
 
-  // 검색 메서드(장르, 대형 지역, 중형 지역 선택 가능)
-  public function searchRestaurantsByLocationCode(?string $genre = null, ?string $large_area = null, ?string $middle_area = null) {
+  /**
+   * 주변 식당 검색 메서드 (장르, 대형 지역, 중 지역 선택 가능)
+   */
+  public function searchRestaurantsByLocationCode(?string $genre = null, ?string $large_area = null, ?string $middle_area = null)
+  {
     $params = [
-      'key' => 'd79ce7978212ebd8',
+      'key' => env('APP_KEY'),
       'format' => 'json',
     ];
 
@@ -45,15 +48,47 @@ class RecruitApiService
     return null;
   }
 
-  // 검색 메서드(가게명)
-  public function searchRestaurantsByKeyword(string $keyword) {
+  /**
+   * FIXME: 가게명으로 검색 메서드
+   */
+  public function searchRestaurantsByKeyword(string $keyword)
+  {
     $params = [
-      'key' => 'd79ce7978212ebd8',
-      'keyword' => $keyword,
+      'key' => env('APP_KEY'),
+      'name' => $name,
       'format' => 'json',
     ];
 
-    $response = $this->client->get('shop/v1/', [
+    // $response = $this->client->get('shop/v1/', [
+    //   'query' => $params
+    // ]);
+
+    // if ($response->getStatusCode() === 200) {
+    //   return json_decode($response->getBody(), true)['results'];
+    // }
+
+    // return null;
+  }
+
+  /**
+   * 사용자 위치기반 주변 식당 검색 메서드
+   */
+  public function searchRestaurantsByUserLocation(float $latitude, float $longitude, float $range, ?string $keyword = null)
+  {
+    $params = [
+      'key' => env('APP_KEY'),
+      'lat' => $latitude,
+      'lng' => $longitude,
+      'range' => $range,
+      'order' => 4,
+      'format' => 'json',
+    ];
+
+    if ($keyword !== null) {
+      $params['keyword'] = $keyword;
+    }
+
+    $response = $this->client->get('gourmet/v1/', [
       'query' => $params
     ]);
 
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Main\Actions\SearchRestaurantsAction;
+use App\Main\Actions\FindNearbyRestaurantsAction;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +16,29 @@ use App\Main\Actions\SearchRestaurantsAction;
 |
 */
 
-Route::get('/search', function(Request $request, SearchRestaurantsAction $action) {
+/**
+ * 가게 검색 기능(장르, 대형 지역, 중형 지역 선택 가능)
+ */
+Route::get('/search', function (Request $request, SearchRestaurantsAction $action) {
     $genre = $request->input('genre');
     $large_area = $request->input('large_area');
     $middle_area = $request->input('middle_area');
 
-    return $action->search($genre, $large_area, $middle_area);
+    return $action($genre, $large_area, $middle_area);
 });
+
+/**
+ * 사용자 위치 기반 가게 검색 기능
+ */
+Route::get('/search/nby', function (Request $request, FindNearbyRestaurantsAction $action) {
+    $lat = floatval($request->input('lat'));
+    $lng = floatval($request->input('lng'));
+
+    $range = 5; // 기본 검색 범위 (5km)
+
+    return $action($lat, $lng, $range);
+});
+
+// TODO: 검색 메서드(가게명)
+// TODO: 사용자 위치 기반으로 가게 추천
+// TODO: 사용자 성향에 따른 가게 추천
