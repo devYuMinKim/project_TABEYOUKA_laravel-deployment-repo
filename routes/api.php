bc3620c14903b6fab956ba3f1ec466c8f0989180
devYuMinKim <gimyumin40@gmail.com>
2023-06-29 21:07:03 +0900

    ✨ feat : Implement logic for retrieving restaurant information based on ID

--- routes/api.php
+++ routes/api.php
@@ -5,48 +5,51 @@ use Illuminate\Support\Facades\Route;
 use App\Search\Actions\SearchRestaurantsAction;
 use App\Search\Actions\FindNearbyRestaurantsAction;
 use App\Recommendation\Actions\RecommendRestaurantsAction;
+use App\Restaurant\Actions\FindRestaurantByIdAction;
 use App\Search\Responders\SearchRestaurantsResponder;
 use App\Search\Responders\FindNearbyRestaurantsResponder;
 use App\Recommendation\Responders\RecommendRestaurantsResponder;
+use App\Restaurant\Responders\FindRestaurantByIdResponder;
 use App\Services\RecruitApiService;
 
 /**
  * 가게 검색 기능(장르, 대형 지역, 중형 지역, 가게명를 선택하여 검색 가능)
  */
-Route::get('/search', function (
-    Request $request, 
-    SearchRestaurantsAction $action, 
+Route::get("/search", function (
+    Request $request,
+    SearchRestaurantsAction $action,
     SearchRestaurantsResponder $responder
-    ) {
-        $genre = $request->input('genre');
-        $large_area = $request->input('large_area');
-        $middle_area = $request->input('middle_area');
-        $keyword = $request->input('name');
+) {
+    $genre = $request->input("genre");
+    $large_area = $request->input("large_area");
+    $middle_area = $request->input("middle_area");
+    $keyword = $request->input("name");
 
-        $result = $action($genre, $large_area, $middle_area, $keyword);
-        return $responder($result);
+    $result = $action($genre, $large_area, $middle_area, $keyword);
+    return $responder($result);
 });
 
 /**
  * 사용자 위치 기반 가게 검색 기능
  */
-Route::get('/search/nby', function (
-    Request $request, FindNearbyRestaurantsAction $action, 
+Route::get("/search/nby", function (
+    Request $request,
+    FindNearbyRestaurantsAction $action,
     FindNearbyRestaurantsResponder $responder
-    ) {
-        $lat = floatval($request->input('lat'));
-        $lng = floatval($request->input('lng'));
+) {
+    $lat = floatval($request->input("lat"));
+    $lng = floatval($request->input("lng"));
 
-        $range = 5; // 기본 검색 범위 (5km)
+    $range = 5; // 기본 검색 범위 (5km)
 
-        $result = $action($lat, $lng, $range);
-        return $responder($result);
+    $result = $action($lat, $lng, $range);
+    return $responder($result);
 });
 
 /**
  * 사용자 성향에 따른 가게 추천 기능
  */
-Route::get('/recommend/{user_id}', function (
+Route::get("/recommend/{user_id}", function (
     string $user_id,
     RecruitApiService $recruitApiService,
     RecommendRestaurantsAction $action,
@@ -54,4 +57,16 @@ Route::get('/recommend/{user_id}', function (
 ) {
     $restaurants = $action($user_id, $recruitApiService);
     return $responder($restaurants);
+});
+
+/**
+ * 가게 아이디로 가게 정보 찾기 기능
+ */
+Route::get("/restaurant/{id}", function (
+    string $id,
+    FindRestaurantByIdAction $action,
+    FindRestaurantByIdResponder $responder
+) {
+    $restaurant = $action($id);
+    return $responder($restaurant);
 });
