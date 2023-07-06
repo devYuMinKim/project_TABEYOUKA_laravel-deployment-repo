c1adb4bed4c674f1aa8106f4b13fcbe2785a71e0
Hyn2 <justin010129@gmail.com>
2023-07-06 17:58:21 +0900

    ✨ feat : Create migration file for user table

--- app/Profile/Domains/Entities/Users.php
+++ app/Profile/Domains/Entities/Users.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
   protected $table = 'users';
+  protected $primaryKey = 'id';
+  public $incrementing = false;
+
+  protected $keyType = 'string';
 
   protected $fillable = ['id', 'nickname', 'profile_image'];
 }
