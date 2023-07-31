bc87ec0ca5342bbdc46f050a153e070738685d0a
Hyn2 <justin010129@gmail.com>
2023-07-31 19:30:05 +0900

    ✨ feat : Modify user auth middleware

--- app/Http/Middleware/VerifyAccessToken.php
+++ app/Http/Middleware/VerifyAccessToken.php
@@ -13,7 +13,7 @@ class VerifyAccessToken
 {
   public function handle(Request $request, Closure $next)
   {
-    $accessToken = $request->access_token;
+    $accessToken = $request->header('Authorization');
     try {
       // token에 대한 유효성을 검증하고 그에 대한 정보를 반환
       $response = Http::get('https://oauth2.googleapis.com/tokeninfo', [
