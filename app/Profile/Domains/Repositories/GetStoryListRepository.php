25ede34a048fe8a1f70c23c71c3dbd752480ff2f
Hyn2 <justin010129@gmail.com>
2024-01-08 20:51:47 +0900

    🐛 fix : Fix join error

--- app/Profile/Domains/Repositories/GetStoryListRepository.php
+++ app/Profile/Domains/Repositories/GetStoryListRepository.php
@@ -13,7 +13,7 @@ class GetStoryListRepository
     $storyList = StoryList::where('user_id', $id)->get();
     foreach ($storyList as $list) {
       // 여기 나중에 리뷰 이미지 테이블 이용해서 불러오도록 바꿔야함.
-      $reviewImage = ReviewImages::where('id', $list->reviews[0]->id)->value('image_url');
+      $reviewImage = ReviewImages::where('review_id', $list->reviews[0]->id)->value('image_url');
       $list->mainImage = $reviewImage;
       // 리뷰까지 한번에 불러와서, 리뷰는 불러오지 않게 제거
       unset($list->reviews);
